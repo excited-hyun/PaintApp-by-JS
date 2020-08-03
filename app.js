4d658fe5fc4399f838438b2273f3fa86e9c57ae3
excited-hyun <nahyun1127@naver.com>
2020-08-03 17:12:11 +0900

    Initial Drawing

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting(){
     painting = false;
 }
 
-function onMouseMove(event){
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){        //마우스 움직이는 내내 발생
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y)
+    if(!painting){
+        console.log("creating path in", x, y);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } 
+    else {
+        console.log("creating line in", x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
